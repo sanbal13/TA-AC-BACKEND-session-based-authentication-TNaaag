84f1e1d11bdc1f123804a35482dd3ddbcafcfad2
Santosh Balchandran <santoshbalchandran@gmail.com>
2021-07-01 00:39:16 +0530

    Assignment- I

---
+++ block-BNaadv/login/models/User.js
@@ -0,0 +1,34 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let bcrypt = require('bcrypt');
+
+let userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, lowercase: true},
+    password: {type: String, minlength: 4}
+}, {timestamps: true});
+
+// Pre save hook
+userSchema.pre('save', function(next) {
+    if(this.password && this.isModified('password')) {
+        bcrypt.hash(this.password, 10, (err, hashed) => {
+            if(err) return next(err);
+            this.password = hashed;
+            next()
+        });
+    } else {
+        next();
+    }
+});
+
+// Verify Password
+userSchema.methods.verifyPassword = function (password, cb) {
+    bcrypt.compare(password, this.password, (err, result) => {
+        return cb(err, result);
+    });
+}
+
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
